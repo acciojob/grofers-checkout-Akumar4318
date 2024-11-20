fbe4646a9d4ee6439f93ab8226305fc6829ef727
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2024-11-21 02:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let price = document.querySelectorAll('.price');
+  let priceArray = [];
+  
+  price.forEach((price1) => {
+      priceArray.push(Number(price1.innerHTML));
+  });
+  console.log(priceArray)
+
+ let ans= priceArray.reduce((totalsum,item)=>{
+return totalsum+item
+  },0)
+
+console.log(ans) 
+
+let newrow=document.createElement('tr');
+let newcell=document.createElement('td')
+newcell.innerHTML=`${ans}`
+
+let table=document.querySelector('table')
+table.appendChild(newrow)
+newrow.appendChild(newcell)
   
 };
 
